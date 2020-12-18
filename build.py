044feaf9064000f8fd7039241aafff089a200be6
Nathan Robinson <nrobinson2000@me.com>
2020-12-18 14:24:29 -0500

    improve demo

--- build.py
+++ build.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
 import neopo
 from subprocess import run, PIPE
 
+# Create version string from commit number and hash
 count = run(["git", "rev-list", "--count", "HEAD"], stdout=PIPE).stdout.splitlines()[0].decode('utf-8')
 commit = run(["git", "rev-parse", "--short", "HEAD"], stdout=PIPE).stdout.splitlines()[0].decode('utf-8')
 VERSION = "%s.%s" % (count, commit)
 
+# Set the RELEASE_STRING flag
 neopo.flags('-DRELEASE_STRING="%s"' % VERSION)
-neopo.build()
+
+# Build or flash firmware
+# neopo.build()
+neopo.flash()
--- src/increment-build.cpp
+++ src/increment-build.cpp
@@ -1,13 +1,18 @@
 #include "Particle.h"
 
-#define STR(x) #x
+#define STR_EXPAND(x) #x
+#define STR(x) STR_EXPAND(x)
 
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
 void setup() {
   Serial.begin(115200);
-  Serial.print(STR(RELEASE_STRING));
+  delay(3000);
+
+  #ifdef RELEASE_STRING
+  Serial.printlnf("Build version: %s", STR(RELEASE_STRING));
+  #endif
 }
 
 void loop() {
